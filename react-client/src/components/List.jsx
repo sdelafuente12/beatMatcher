3cdf4fa939c2ee1ac075c223d68ad2a797e695a9
sdelafuente12 <s.delafuente831@gmail.com>
2019-08-30 17:25:41 -0500

    added hyphen for top5

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 
 import ListItem from './ListItem.jsx';
 
-
 // const List = ({ matchingTracks }) => (
 const List = ({ matchingTracks, top5 }) => (
   <div>
     <h3>top 5 tracks of the day</h3>
-    <ol> { top5.map(track => <li> {track.title}- {track.artist} </li>) } </ol>
+    <ol> { top5.map(track => <li> {track.title} - {track.artist} </li>) } </ol>
     <h4> YR MATCHES!! </h4>
     There are { matchingTracks.length } matchingTracks.
     { matchingTracks.map(track => <ListItem track={track} />)}
