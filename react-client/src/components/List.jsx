d08156b8719894ed1051bf106d06e3696e8610de
sdelafuente12 <s.delafuente831@gmail.com>
2019-08-30 17:16:18 -0500

    passed database results to list

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import ListItem from './ListItem.jsx';
 
 
 // const List = ({ matchingTracks }) => (
-const List = ({ matchingTracks }) => (
+const List = ({ matchingTracks, top5 }) => (
   <div>
+    <h3>top 5 tracks of the day</h3>
+    <ol> { top5.map(track => <li> {track.title}- {track.artist} </li>) } </ol>
     <h4> YR MATCHES!! </h4>
     There are { matchingTracks.length } matchingTracks.
     { matchingTracks.map(track => <ListItem track={track} />)}
